fc66930303652d8f4377470c9dcb544f73090797
kochamklawiatury <wi3rzbicki.piotr@gmail.com>
2024-02-22 18:49:25 +0100

    brak trzęsień ziemii XD

---
+++ script.js
@@ -0,0 +1,115 @@
+
+const canvas = document.getElementById("root");
+const svgNS = "http://www.w3.org/2000/svg" ;
+const offset = (()=>{
+    canvasRect = canvas.getBoundingClientRect();
+    return {x:canvasRect.x, y:canvasRect.y};
+})();
+
+let circles = []
+let gravity = 1
+
+
+canvas.onclick = event => {
+    let position = {x:event.x, y:event.y};
+    let circle = document.createElementNS(svgNS, "circle");
+    circle.setAttribute( "cx", position.x-offset.x);
+    circle.setAttribute( "cy", position.y-offset.y);
+    circle.setAttribute( "r", 10);
+    circle.setAttribute( "fill", "red");
+    circle.vx = (Math.random()-0.5)*200;
+    circle.vy = (Math.random()-0.5)*200;
+    circle.onGround = false
+    circles.push(circle);
+    canvas.appendChild(circle);
+}
+let predkosc = 20
+let lastUpdate;
+function selectPlanet (planetGravity){
+    gravity = planetGravity
+}
+
+function updateCircle() {
+
+    if(lastUpdate == undefined){
+        lastUpdate = new Date ();
+        return;
+    }
+    let currentTime = new Date();
+    let deltaTime  = currentTime - lastUpdate;
+    lastUpdate = currentTime
+    
+    dumping = 0.8
+    // for (ball of circles) {
+    //     ball.fx = 0
+    //     ball.fy = 0 
+    // }
+
+    // for (ball of circles) {
+    //     for (other of circles){
+    //         dx = other.x - currentX
+    //         dy = other.y - currentY
+    //         d2 = dx^2 + dy^2
+    //         d = Math.sqrt(d2)
+    //         F = G/d2
+    //         Fx = F * (dx/d)
+    //         Fy = F * (dy/d)
+    //         ball.fx += Fx
+    //         ball.fy += Fy 
+    //     }
+    // }
+    
+    // for (ball of circles){
+    //         ax = ball.fx
+    //         vx = ax * deltaTime
+            
+    // }
+
+    for(let circle of circles){
+        currentX = parseFloat(circle.getAttribute("cx")) + circle.vx*(deltaTime/1000);
+        currentY = parseFloat(circle.getAttribute("cy")) + circle.vy*(deltaTime/1000);
+        r = parseFloat(circle.getAttribute("r"))
+         
+        if(currentX +  r > canvas.clientWidth){
+             currentX = 2*canvas.clientWidth - (currentX + r) - r;
+             circle.vx = -circle.vx * dumping
+         }
+
+         if(currentY  + r >= canvas.clientHeight){
+             currentY = 2*canvas.clientHeight - (currentY + r) - r;
+             circle.vy = -circle.vy * dumping
+             if (Math.abs(circle.vy)<50) {
+                circle.vy = 0
+                currentY = canvas.clientHeight - r
+                circle.onGround = true
+             }
+         }
+
+         if(currentX - r < 0){
+            currentX = - (currentX - r) + r;
+            circle.vx = -circle.vx
+        }
+
+        if(currentY - r <0){
+            currentY = - (currentY - r) + r;
+            circle.vy = -circle.vy
+        }
+
+  
+        if (circle.onGround == false) {
+            circle.vy = circle.vy + deltaTime * gravity * 0.3
+        } else {
+            circle.vx = circle.vx * (1 - gravity*0.001)
+            if (Math.abs(circle.vx)<0.1) {
+                circle.vx = 0
+            }
+        }
+        
+        //circle.vx = circle.vx + deltaTime * gravity + circle.fy
+
+        circle.setAttribute("cx", currentX);
+        circle.setAttribute("cy", currentY);
+    }
+}
+
+setInterval(updateCircle, 5)
